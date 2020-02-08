3e6fb9f6cb6d6f0e5fef0ff228e37c0273410c3e
Joe Fearnley <joe.fearnley@gmail.com>
2020-02-08 09:10:24 -0500

    starting over

--- choring/chores/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Chore(models.Model):
-    name = models.CharField(max_length=256)
-    assignee = models.IntegerField()
-    due_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
