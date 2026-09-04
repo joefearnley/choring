be2804ff85225acd54d5bcbebde64d7f720522f8
Joe Fearnley <joe.fearnley@gmail.com>
2026-09-04 15:33:27 -0400

    Apply ruff auto-fixes

--- choring/chores/serializers.py
+++ choring/chores/serializers.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
