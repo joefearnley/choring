efbf7630c3c8acbeb39b49673fbab7e544583be7
Joe Fearnley <joe.fearnley@gmail.com>
2026-09-07 10:50:38 -0400

    Setting up DEFAULT_AUTO_FIELD

--- choring/settings.py
+++ choring/settings.py
@@ -140,3 +140,5 @@ DEFAULT_FROM_EMAIL = 'no-reply@localhost'
 # Authentication redirects
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
